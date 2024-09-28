4f455b48afe20f20a1ec77af4f32d4dd5cce378f
HoangTung-Vu <minhhoangtungvu04@gmail.com>
2024-09-28 11:32:35 +0700

    sori

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="sns_KAN",  # The name people will use to install your package
     version="0.0.3",
     packages=find_packages(),  # Automatically find the simpekan package
-    install_requires=['pykan', 'pytorch'],  # Add any dependencies here
+    install_requires=['pykan'],  # Add any dependencies here
     author="Hoang Tung M Vu, Pham Ngoc Do",
     author_email="minnhhoangtungvu04@gmail.com",
     description="A simple KAN library with wavelet KAN",
