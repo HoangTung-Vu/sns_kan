b07d50e63851ff212cdc1473a85d205bcb9e02a4
HoangTung-Vu <minhhoangtungvu04@gmail.com>
2024-09-28 11:17:30 +0700

    coauthor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.3",
     packages=find_packages(),  # Automatically find the simpekan package
     install_requires=['pykan', 'pytorch'],  # Add any dependencies here
-    author="Hoang Tung",
+    author="Hoang Tung M Vu, Pham Ngoc Do",
     author_email="minnhhoangtungvu04@gmail.com",
     description="A simple KAN library with wavelet KAN",
     url="https://github.com/HoangTung-Vu/sns_kan",  # Link to the repository
